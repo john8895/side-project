fb29a55b44b81a86092cc006a187e37efefc4f92
JohnChen <paiching01@gmail.com>
2020-12-11 12:26:51 +0800

    update

--- js/all.js
+++ js/all.js
@@ -58,3 +58,40 @@ $(window).on('load', function () {
     });
 
 });
+if(document.getElementById('app')) {
+    const em = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello world!'
+        },
+        created: function(){
+            this.indexSlide()
+        },
+        methods: {
+            indexSlide: function (){
+                console.log(555)
+                var mySwiper = new Swiper('.swiper-container', {
+                    // Optional parameters
+                    direction: 'vertical',
+                    loop: true,
+
+                    // If we need pagination
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+
+                    // Navigation arrows
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+
+                    // And if we need scrollbar
+                    scrollbar: {
+                        el: '.swiper-scrollbar',
+                    },
+                })
+            }
+        }
+    })
+}
